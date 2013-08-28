6fba950ec9a0b485e0cd085d9fddb45df671422b
Wouter R <gennerate@zonnet.nl>
2013-08-28 19:02:21 +0200

    Add (almost empty) lua binding; raw sketch of program flow in print3d.cpp.

---
+++ src/frontend/lua/fe_lua.c
@@ -0,0 +1,41 @@
+#define LUA_LIB
+#include <math.h> /* TEMP for l_cos() */
+#include "lua.h"
+#include "lauxlib.h"
+
+static int l_cos(lua_State *L) {
+	double d = luaL_checknumber(L, 1);
+	lua_pushnumber(L, cos(d));
+	return 1;
+}
+
+//make all printer-specific calls also usable with the printer:call() syntax (see libuci-lua)
+//write code such that printer IDs never have to be passed (always default to first (and probably only) printer found)
+
+//number getNumPrinters()
+//table getPrinterIDs()
+//table getPrinterDetails(id?)
+
+//number getGcodeBufferSize(id?)
+//void clearGcodeBuffer(id?)
+//--bool appendToGcodeBuffer(id?) --kan dus niet (teveel data), in plaats daarvan:
+//stream getGcodeBufferAppendStream(id?)
+//bool printGcodeBuffer(id?)
+//bool isPrintingGcodeBuffer(id?) //--weglaten en getPos <0 laten teruggeven bij niet printen?
+//number getGcodeBufferPrintPosition(id?, bool relative) //--absoluut, of /size -> 0-1 bereik
+//bool stopGcodeBufferPrinting(id?)
+
+//bool sendGcode(id?, data)
+
+//number getTemperature(id?)
+//...
+
+static const struct luaL_Reg print3dlib[] = {
+		{"p3dcos", l_cos},
+		{NULL, NULL}
+};
+
+int luaopen_print3dlib(lua_State *L) {
+	luaL_newlib(L, print3dlib);
+	return 1;
+}
--- src/print3d.cpp
+++ src/print3d.cpp
@@ -1,14 +1,27 @@
 #include <stdlib.h>
 #include "Logger.h"
+#include "Serial.h"
 
 int main(int argc, char** argv) {
+	const char* serialDevice = "/dev/ttyACM0";
+
+	//handle cmdline args
+	//open logger
+	//open fifo/file for IPC
+	//fork (as late as possible but before any possibly lengthy operations like serial port things)
+	//open port
+	//set (detect?) speed
+
+	//wait for commands
+	//  if no printer object yet, or type config changed, create printer object
+	//  dispatch request to printer object
+
 	Logger l;
 	l.open(stderr, Logger::BULK);
 
-	//handle cmdline args
-	//serial open
+	Serial s;
+	s.open(serialDevice);
 
-	//serial close
-
+	s.close();
 	exit(0);
 }
