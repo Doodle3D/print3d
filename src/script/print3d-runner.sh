4c43f11f6695b4712ecd97baf5c60422d21163d0
Wouter R <gennerate@zonnet.nl>
2016-02-23 08:48:57 +0100

    Logging: remove temporary code for specifying log level to print3d server in runner script (it now reads required information from UCI itself). Addresses https://github.com/Doodle3D/print3d/issues/37.

--- src/script/print3d-runner.sh
+++ src/script/print3d-runner.sh
@@ -5,27 +5,22 @@
 # This software is licensed under the terms of the GNU GPL v2 or later.
 # See file LICENSE.txt or visit http://www.gnu.org/licenses/gpl.html for full license details.
 
-#Note: temporary way to have print3d respect log level specified in UCI (print3d will read this internally later on)
-LOG_LEVEL=`uci get wifibox.general.system_log_level`
-LOG_FLAG=
-case $LOG_LEVEL in
-	quiet) LOG_FLAG=-Q ;;
-	error) LOG_FLAG=-q ;;
-	warning) ;;
-	info) ;;
-	verbose) LOG_FLAG=-v ;;
-	bulk) LOG_FLAG=-V ;;
-esac
-
 PRINT3D_RES_PATH=/tmp
 PRINT3D=/bin/print3d
 DEVBASE=`basename $1`
-OPTIONS=$LOG_FLAG
+
+#Options:
+# -F: start server without forking (this script is backgrounded by print3d-manager)
+# -u: read settings from UCI (mainly log location and level)
+# -d ${DEVBASE}: which device to use for communication with the printer
+OPTIONS="-u -F -d ${DEVBASE}"
 
 sleep 5
 
-#start server without forking (this script is backgrounded by print3d-manager)
-$PRINT3D -F -d ${DEVBASE} $OPTIONS >> $PRINT3D_RES_PATH/print3d-${DEVBASE}.log 2>&1
+#Note: we do not redirect output anymore; except for a few startup messages (which
+#can be seen when running the server manually), everything can be found in the log file.
+$PRINT3D $OPTIONS
+#$PRINT3D $OPTIONS >> $PRINT3D_RES_PATH/print3d-${DEVBASE}.log 2>&1
 
 #remove socket (print3d-manager uses this to detect if a server is running for a certain device)
 rm $PRINT3D_RES_PATH/print3d-${DEVBASE}
